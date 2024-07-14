43602a12d469c9d638e8a5554a8f10295addf538
honghoker <ekflzhftk@naver.com>
2024-07-15 01:17:29 +0900

    [Fix] 각 모듈에 대한 TargetDependency 수정
    - Target 이름이 잘못 지정되어 있던 문제 수정
    - 사용되지 않을 코드 제거

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/TargetDependency+Modules.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/TargetDependency+Modules.swift
@@ -23,34 +23,9 @@ public extension TargetDependency {
 // MARK: TargetDependency + Presentation
 
 public extension TargetDependency {
-  static var presentation: Self {
-    return .project(target: ModulePath.Presentation.name, path: .presentation)
-  }
-  
   static func presentation(implements module: ModulePath.Presentation) -> Self {
     return .project(
-      target: ModulePath.Presentation.name + module.rawValue,
-      path: .presentation(implementation: module)
-    )
-  }
-  
-  static func presentation(interface module: ModulePath.Presentation) -> Self {
-    return .project(
-      target: ModulePath.Presentation.name + module.rawValue + "Interface",
-      path: .presentation(implementation: module)
-    )
-  }
-  
-  static func presentation(tests module: ModulePath.Presentation) -> Self {
-    return .project(
-      target: ModulePath.Presentation.name + module.rawValue + "Tests",
-      path: .presentation(implementation: module)
-    )
-  }
-  
-  static func presentation(testing module: ModulePath.Presentation) -> Self {
-    return .project(
-      target: ModulePath.Presentation.name + module.rawValue + "Testing",
+      target: module.rawValue,
       path: .presentation(implementation: module)
     )
   }
@@ -59,34 +34,9 @@ public extension TargetDependency {
 // MARK: TargetDependency + Domain
 
 public extension TargetDependency {
-  static var domain: Self {
-    return .project(target: ModulePath.Domain.name, path: .domain)
-  }
-  
   static func domain(implements module: ModulePath.Domain) -> Self {
     return .project(
-      target: ModulePath.Domain.name + module.rawValue,
-      path: .domain(implementation: module)
-    )
-  }
-  
-  static func domain(interface module: ModulePath.Domain) -> Self {
-    return .project(
-      target: ModulePath.Domain.name + module.rawValue + "Interface",
-      path: .domain(implementation: module)
-    )
-  }
-  
-  static func domain(tests module: ModulePath.Domain) -> Self {
-    return .project(
-      target: ModulePath.Domain.name + module.rawValue + "Tests",
-      path: .domain(implementation: module)
-    )
-  }
-  
-  static func domain(testing module: ModulePath.Domain) -> Self {
-    return .project(
-      target: ModulePath.Domain.name + module.rawValue + "Testing",
+      target: module.rawValue,
       path: .domain(implementation: module)
     )
   }
@@ -95,34 +45,9 @@ public extension TargetDependency {
 // MARK: TargetDependency + Core
 
 public extension TargetDependency {
-  static var core: Self {
-    return .project(target: ModulePath.Core.name, path: .core)
-  }
-  
   static func core(implements module: ModulePath.Core) -> Self {
     return .project(
-      target: ModulePath.Core.name + module.rawValue,
-      path: .core(implementation: module)
-    )
-  }
-  
-  static func core(interface module: ModulePath.Core) -> Self {
-    return .project(
-      target: ModulePath.Core.name + module.rawValue + "Interface",
-      path: .core(implementation: module)
-    )
-  }
-  
-  static func core(tests module: ModulePath.Core) -> Self {
-    return .project(
-      target: ModulePath.Core.name + module.rawValue + "Tests",
-      path: .core(implementation: module)
-    )
-  }
-  
-  static func core(testing module: ModulePath.Core) -> Self {
-    return .project(
-      target: ModulePath.Core.name + module.rawValue + "Testing",
+      target: module.rawValue,
       path: .core(implementation: module)
     )
   }
@@ -131,20 +56,9 @@ public extension TargetDependency {
 // MARK: TargetDependency + Shared
 
 public extension TargetDependency {
-  static var shared: Self {
-    return .project(target: ModulePath.Shared.name, path: .shared)
-  }
-  
   static func shared(implements module: ModulePath.Shared) -> Self {
     return .project(
-      target: ModulePath.Shared.name + module.rawValue,
-      path: .shared(implementation: module)
-    )
-  }
-  
-  static func shared(interface module: ModulePath.Shared) -> Self {
-    return .project(
-      target: ModulePath.Shared.name + module.rawValue + "Interface",
+      target: module.rawValue,
       path: .shared(implementation: module)
     )
   }
@@ -153,13 +67,9 @@ public extension TargetDependency {
 // MARK: TargetDependency + DesignSystem
 
 public extension TargetDependency {
-  static var designSystem: Self {
-    return .project(target: ModulePath.DesignSystem.name, path: .designSystem)
-  }
-  
   static func designSystem(implements module: ModulePath.DesignSystem) -> Self {
     return .project(
-      target: ModulePath.DesignSystem.name + module.rawValue,
+      target: module.rawValue,
       path: .designSystem(implementation: module)
     )
   }
