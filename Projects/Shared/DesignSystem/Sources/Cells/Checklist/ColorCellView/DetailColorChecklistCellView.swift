ddc86ccbd86a0a84f353c079735f6a009f4e6564
honghoker <ekflzhftk@naver.com>
2024-12-11 02:12:15 +0900

    [Feat] #35 - 체크리스트 뷰 컴포넌트 swipe to delete action 추가

--- Projects/Shared/DesignSystem/Sources/Cells/Checklist/ColorCellView/DetailColorChecklistCellView.swift
+++ Projects/Shared/DesignSystem/Sources/Cells/Checklist/ColorCellView/DetailColorChecklistCellView.swift
@@ -11,15 +11,18 @@ public struct DetailColorChecklistCellView: View {
   private let title: String
   private let isSelected: Bool
   private let onToggle: () -> Void
+  private let onDelete: () -> Void
   
   public init(
     title: String,
     isSelected: Bool,
-    onToggle: @escaping () -> Void
+    onToggle: @escaping () -> Void,
+    onDelete: @escaping () -> Void
   ) {
     self.title = title
     self.isSelected = isSelected
     self.onToggle = onToggle
+    self.onDelete = onDelete
   }
   
   public var body: some View {
@@ -29,5 +32,16 @@ public struct DetailColorChecklistCellView: View {
       type: .detail,
       onToggle: onToggle
     )
+    .listRowBackground(Color.clear)
+    .listRowSeparator(.hidden)
+    .listRowInsets(.init(top: 0, leading: 12, bottom: 0, trailing: 12))
+    .swipeActions(edge: .trailing) {
+      Button {
+        onDelete()
+      } label: {
+        Image.trash
+      }
+      .tint(.red)
+    }
   }
 }
