c748dd8c6a71f563b09406adfb7c206b5f280333
honghoker <sinbein9@gmail.com>
2025-03-15 03:20:32 +0900

    [Feat] #51 - 아티클 버튼 메뉴 컴포넌트 구현

---
+++ Projects/Core/Domain/Sources/Models/ArticleCategory.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleCategory.swift
+//  CoreDomain
+//
+//  Created by eunpyo on 3/15/25.
+//
+
+import Foundation
+
+public enum ArticleCategory: CaseIterable {
+  case all
+  case development
+  case plan
+  case design
+}
