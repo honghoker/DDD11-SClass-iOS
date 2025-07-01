6ff241e3d5892b5527df249c8cc6bbb39b8e0957
honghoker <sinbein9@gmail.com>
2025-07-02 02:33:39 +0900

    [Fix] SharedDesignSystem 의존성 문제 수정
    thirdPartyLib 의존성 추가가 되지 않아 "Could not build Objective-C module" 에러가 발생하는 문제 수정

--- Projects/Core/Network/Sources/Services/Article/ArticleAPIClient.swift
+++ Projects/Core/Network/Sources/Services/Article/ArticleAPIClient.swift
@@ -25,7 +25,7 @@ public extension DependencyValues {
 
 extension ArticleAPIClient: DependencyKey {
   public static var liveValue: ArticleAPIClient = .init(
-    fetchArticles: { reqeust in
+    _fetchArticles: { reqeust in
       let api = ArticleAPI.fetchArticles(
         .init(
           categoryId: reqeust.category?.id,
@@ -40,7 +40,7 @@ extension ArticleAPIClient: DependencyKey {
   )
   
   public static var testValue: ArticleAPIClient = .init(
-    fetchArticles: { _ in
+    _fetchArticles: { _ in
       return Article.mockArticles
     }
   )
--- Projects/Shared/DesignSystem/Project.swift
+++ Projects/Shared/DesignSystem/Project.swift
@@ -9,7 +9,7 @@ let project = Project.makeModule(
   product: .staticFramework,
   settings: .settings(),
   dependencies: [
-    
+    .shared(implements: .thirdPartyLib)
   ],
   sources: ["Sources/**"],
   resources: ["Resources/**"]
