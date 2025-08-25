874f240f7d784a77e54f3eafc2dda82c11fb9ec4
honghoker <sinbein9@gmail.com>
2025-08-25 23:54:03 +0900

    [Feat] #66 - 메인 아티클 API 응답에서 오지않던 필드 값들 추가
    카테고리, 출처, 날짜 추가

--- Projects/Core/Network/Sources/Services/Home/Response/MainArticleResponseDTO.swift
+++ Projects/Core/Network/Sources/Services/Home/Response/MainArticleResponseDTO.swift
@@ -15,21 +15,16 @@ public struct MainArticleResponseDTO: Decodable {
   let thumbnail: String
   let url: String
   let views: Int
-  
-  enum CodingKeys: String, CodingKey {
-    case title
-    case summary
-    case thumbnail
-    case url
-    case views
-  }
-  
+  let source: String
+  let categoryName: String?
+  let postDate: String
+
   var toEntity: MainArticle {
     return MainArticle(
       title: title,
-      category: "디자인 시스템", // TODO: API 에서 안옴
-      source: "채널톡", // TODO: API에서 안옴
-      postDate: Date(), // TODO: API에서 안옴
+      category: categoryName ?? "",
+      source: source,
+      postDate: ISO8601DateFormatter().date(from: postDate) ?? .init(),
       thumbnailURL: thumbnail,
       url: url
     )
